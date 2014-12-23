c375ae4932517d6a50aaea5cc2a44209cadff96f
mohammed2049 <mohammed.shane94@gmail.com>
2014-12-23 20:44:45 +0200

    Add_address_to_Memory

--- OS#4_ASSI/src/Main.java
+++ OS#4_ASSI/src/Main.java
@@ -4,16 +4,25 @@ public class Main {
 	public static ArrayList<Memory> partitions = new ArrayList<Memory>();
 
 	public static void main(String[] args) {
-		int n, sz;
+		int n, sz , address = 1;
 		Scanner input = new Scanner(System.in);
 		System.out.print("Number of partitions : \n");
 		n = input.nextInt();
 		System.out.println("Size of each partition : ");
 		for (int i = 0; i < n; ++i) {
 			sz = input.nextInt();
-			Memory e = new Memory(sz, false);
+			Memory e = new Memory(sz, false , address);
 			partitions.add(e);
+			address += sz;
 		}
+		
+		
+		for(int i = 0 ; i < partitions.size() ; ++i){
+			Memory e = new Memory();
+			e = partitions.get(i);
+			System.out.println(i + " " + e.getSz() + " " + e.getAddress());
+		}
+		
 		input.close();
 	}
 
--- OS#4_ASSI/src/Memory.java
+++ OS#4_ASSI/src/Memory.java
@@ -1,15 +1,16 @@
 public class Memory {
 	private int Sz;
 	private boolean z;
-
+	private int address;
 	Memory() {
 		setSz(0);
 		setZ(false);
 	}
 
-	Memory(int s, boolean a) {
+	Memory(int s, boolean a , int address) {
 		Sz = s;
 		z = a;
+		this.address = address;
 	}
 
 	public int getSz() {
@@ -26,5 +27,13 @@ public class Memory {
 
 	public void setZ(boolean z) {
 		this.z = z;
+	}
+
+	public int getAddress() {
+		return address;
+	}
+
+	public void setAddress(int address) {
+		this.address = address;
 	}
 }
